e4bc578a673a950bf9ed835c4be5847f023db1c3
E-Jacques <emil00j77@gmail.com>
2023-11-28 22:58:40 +0100

    feat(sort-cli-command) skip first element of env::args

--- src/main.rs
+++ src/main.rs
@@ -2,6 +2,6 @@ use rs_file_sorter::handle;
 use std::env;
 
 fn main() {
-    let input = env::args().collect::<Vec<String>>().join(" ");
+    let input = env::args().skip(1).collect::<Vec<String>>().join(" ");
     handle(input, None);
 }
