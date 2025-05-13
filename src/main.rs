7897bb612a7396b989fb733f62b84d55af1ac5be
E-Jacques <emil00j77@gmail.com>
2025-05-13 22:50:21 +0200

    feat(cli): create specific binary for cli

--- src/main.rs
+++ src/main.rs
@@ -1,8 +1,3 @@
-use std::env;
-
-use rs_file_sorter::cli::handle;
-
 fn main() {
-    let input = env::args().skip(1).collect::<Vec<String>>().join(" ");
-    handle(input, None);
+    panic!("Please use the rsfs_cli binaries");
 }
