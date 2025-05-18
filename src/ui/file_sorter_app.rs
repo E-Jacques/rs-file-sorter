dccd5f3cd06bc2488d807baf637c63c2d535add5
E-Jacques <emil00j77@gmail.com>
2025-05-18 21:59:36 +0200

    feat(ui): add folder selection button

--- src/ui/file_sorter_app.rs
+++ src/ui/file_sorter_app.rs
@@ -1,14 +1,15 @@
 use iced::{
-    widget::{button, column, container, text_input},
+    widget::{button, column, container, row, text_input},
     Element, Length,
 };
+use rfd::FileDialog;
 
 use crate::{
     core::{sorter, sorting_strategy::SortingStrategy},
     utils::logger::Logger,
 };
 
-use super::widgets::editable_file_tree;
+use super::widgets::{editable_file_tree, icon};
 
 #[derive(Default)]
 pub struct FileSorterApp<'a> {
@@ -19,9 +20,16 @@ pub struct FileSorterApp<'a> {
 }
 
 #[derive(Debug, Clone)]
+pub enum PathType {
+    Input,
+    Output,
+}
+
+#[derive(Debug, Clone)]
 pub enum Message {
     InputPathChanged(String),
     OutputPathChanged(String),
+    OpenDirectorySelector(PathType),
     EditableFileTreeMessage(editable_file_tree::Message),
     Sort,
 }
@@ -40,16 +48,27 @@ impl<'a> FileSorterApp<'a> {
     }
 
     pub fn view(&self) -> Element<'_, Message> {
-        let input_path =
-            text_input("Input to sort", &self.input_path).on_input(Message::InputPathChanged);
-        let output_path =
-            text_input("Output path", &self.output_path).on_input(Message::OutputPathChanged);
+        let input_path = row![
+            text_input("Input to sort", &self.input_path).on_input(Message::InputPathChanged),
+            button(icon::icon(icon::FOLDER_CLOSED))
+                .on_press(Message::OpenDirectorySelector(PathType::Input))
+        ]
+        .spacing(8);
+        let output_path = row![
+            text_input("Output path", &self.output_path).on_input(Message::OutputPathChanged),
+            button(icon::icon(icon::FOLDER_CLOSED))
+                .on_press(Message::OpenDirectorySelector(PathType::Output))
+        ]
+        .spacing(8);
+
         let output_path_tree = self
             .editable_file_tree
             .view()
             .map(move |msg| Message::EditableFileTreeMessage(msg));
-        let button = button("Sort").on_press(Message::Sort).width(Length::Fill);
-        let content = column![input_path, output_path, output_path_tree, button]
+
+        let sort_button = button("Sort").on_press(Message::Sort).width(Length::Fill);
+
+        let content = column![input_path, output_path, output_path_tree, sort_button]
             .padding(20)
             .spacing(10);
 
@@ -73,6 +92,20 @@ impl<'a> FileSorterApp<'a> {
             Message::EditableFileTreeMessage(m) => {
                 self.editable_file_tree.update(m);
                 self.sorting_strategies = self.editable_file_tree.get_sorting_strategies();
+            }
+            Message::OpenDirectorySelector(path_type) => {
+                let files = FileDialog::new().set_directory("/").pick_folder();
+                if let Some(path) = files {
+                    let path = String::from(path.to_str().unwrap());
+                    match path_type {
+                        PathType::Input => {
+                            self.input_path = path;
+                        }
+                        PathType::Output => {
+                            self.output_path = path;
+                        }
+                    }
+                }
             }
         }
     }
