4b23493e693ea94e22f4cab457100b048c3672ad
E-Jacques <emil00j77@gmail.com>
2025-05-21 08:05:39 +0200

    feat(ui): change window size

--- src/ui/mod.rs
+++ src/ui/mod.rs
@@ -1,4 +1,5 @@
 use file_sorter_app::FileSorterApp;
+use iced::Size;
 
 mod file_sorter_app;
 mod widgets;
@@ -6,6 +7,7 @@ mod widgets;
 pub fn run_app() {
     let _ =
         iced::application::application("File sorter", FileSorterApp::update, FileSorterApp::view)
+            .window_size(Size::new(400.0, 600.0))
             .font(widgets::icon::FONT)
             .run();
 }
