3fa48228c8cc7c405661e3019809f82cc96dcca8
E-Jacques <emil00j77@gmail.com>
2025-05-24 08:22:48 +0200

    refactor(strategy): use new static function to create strategies

--- src/sorting_strategies/concat_strategy.rs
+++ src/sorting_strategies/concat_strategy.rs
@@ -1,18 +1,18 @@
-use std::{fs::File, sync::Arc};
+use std::fs::File;
 
 use crate::core::sorting_strategy::SortingStrategy;
 
 pub fn concat_strategy(strategies: Vec<SortingStrategy>) -> SortingStrategy {
     let borrowed_strategies: Vec<SortingStrategy> = strategies.clone();
-    SortingStrategy {
-        name: String::from("concat"),
-        action: Arc::new(Box::new(move |f: &File| {
+    SortingStrategy::new(
+        "concat",
+        Box::new(move |f: &File| {
             let mut result = String::new();
             for strategy in &borrowed_strategies {
                 let action = &strategy.action;
                 result.push_str(&action(f));
             }
             result
-        })),
-    }
+        }),
+    )
 }
