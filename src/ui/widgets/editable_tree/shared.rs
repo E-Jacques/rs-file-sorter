0e0272af0cf0f81fef9a5d5c8c4a218dabcc87db
E-Jacques <emil00j77@gmail.com>
2025-05-29 08:24:55 +0200

    refactor(ui): use trait for editable tree item

--- src/ui/widgets/editable_tree/shared.rs
+++ src/ui/widgets/editable_tree/shared.rs
@@ -1,3 +1,12 @@
-use iced::widget::combo_box;
+use iced::{widget::combo_box, Element};
+
+use crate::core::sorting_strategy::SortingStrategy;
 
 pub type StrategyOptions = combo_box::State<String>;
+
+pub trait TreeItem<M>: std::fmt::Debug {
+    fn view(&self) -> Element<'_, M>;
+    fn update(&mut self, msg: M);
+    fn box_clone(&self) -> Box<dyn TreeItem<M>>;
+    fn get_sorting_strategy(&self) -> Option<SortingStrategy>;
+}
