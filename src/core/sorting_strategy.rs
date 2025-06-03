2b60fff90491051f00a0fdcdbf2efd6b4376792e
E-Jacques <emil00j77@gmail.com>
2025-06-03 08:35:15 +0200

    feat(core): add parameter support for strategy
    Implement parameter support for sorting strategy and, in the same time, change the expected function type to remove the Fn trait implementation and Arc.

--- src/core/sorting_strategy.rs
+++ src/core/sorting_strategy.rs
@@ -1,12 +1,19 @@
+use std::collections::HashMap;
 use std::fs::File;
 use std::sync::{Arc, Mutex};
 
-type SortingStrategyAction = Box<dyn Fn(&File) -> String>;
+type SortingStrategyAction = fn(&File, &HashMap<String, StrategyParameter>) -> String;
+
+#[derive(Clone)]
+pub enum StrategyParameter {
+    Strategy(Vec<Box<SortingStrategy>>),
+}
 
 #[derive(Clone)]
 pub struct SortingStrategy {
-    pub action: Arc<SortingStrategyAction>,
+    pub action: SortingStrategyAction,
     pub name: String,
+    pub parameters: HashMap<String, StrategyParameter>,
 }
 
 impl std::fmt::Debug for SortingStrategy {
@@ -20,9 +27,14 @@ impl std::fmt::Debug for SortingStrategy {
 impl SortingStrategy {
     pub fn new(name: &str, action: SortingStrategyAction) -> SortingStrategy {
         SortingStrategy {
-            action: Arc::new(action),
+            action: action,
             name: name.to_string(),
+            parameters: HashMap::new(),
         }
+    }
+
+    pub fn add_parameter(&mut self, key: String, value: StrategyParameter) {
+        self.parameters.insert(key, value);
     }
 
     pub(crate) fn apply(&self, file: &File) -> String {
@@ -30,7 +42,7 @@ impl SortingStrategy {
         let file_mutex = Arc::new(Mutex::new(file));
         let file_clone = Arc::clone(&file_mutex);
         let file_lock = file_clone.lock().unwrap();
-        let result = (self.action)(&*file_lock);
+        let result = (self.action)(&*file_lock, &self.parameters.clone());
         println!("{:#?}", result);
 
         return result;
