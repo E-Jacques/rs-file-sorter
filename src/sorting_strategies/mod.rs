53d5b36e91907eb01e4ff7550550af8c32edf4c4
E-Jacques <emil00j77@gmail.com>
2025-06-03 08:37:30 +0200

    feat(strategy): handle parameter support
    Add the parameter argument to the closure when creating a new instance of SortingStrategy. Also remove the box creation to fit the new expected input.

--- src/sorting_strategies/mod.rs
+++ src/sorting_strategies/mod.rs
@@ -11,45 +11,39 @@ use crate::{
 pub mod concat_strategy;
 
 pub fn get_month_sorting_strategy() -> SortingStrategy {
-    SortingStrategy::new(
-        "month",
-        Box::new(|f: &File| match get_month_number(f) {
-            Ok(month_number) => {
-                let french_month_name = vec![
-                    "Janvier",
-                    "Février",
-                    "Mars",
-                    "Avril",
-                    "Mai",
-                    "Juin",
-                    "Juillet",
-                    "Août",
-                    "Septembre",
-                    "Octobre",
-                    "Novembre",
-                    "Décembre",
-                ];
+    SortingStrategy::new("month", |f: &File, _| match get_month_number(f) {
+        Ok(month_number) => {
+            let french_month_name = vec![
+                "Janvier",
+                "Février",
+                "Mars",
+                "Avril",
+                "Mai",
+                "Juin",
+                "Juillet",
+                "Août",
+                "Septembre",
+                "Octobre",
+                "Novembre",
+                "Décembre",
+            ];
 
-                let vec_index: usize = (month_number - 1).try_into().unwrap();
-                format!(
-                    "{}_{}",
-                    add_0_to_single_number(month_number),
-                    french_month_name
-                        .get(vec_index)
-                        .expect("The month of the file shouldn't exceed 12 !")
-                )
-            }
-            Err(error) => panic!("{}", format!("Cannot retrieve month number: {:#?}", error)),
-        }),
-    )
+            let vec_index: usize = (month_number - 1).try_into().unwrap();
+            format!(
+                "{}_{}",
+                add_0_to_single_number(month_number),
+                french_month_name
+                    .get(vec_index)
+                    .expect("The month of the file shouldn't exceed 12 !")
+            )
+        }
+        Err(error) => panic!("{}", format!("Cannot retrieve month number: {:#?}", error)),
+    })
 }
 
 pub fn get_year_sorting_strategy() -> SortingStrategy {
-    SortingStrategy::new(
-        "year",
-        Box::new(|f: &File| match get_year_number(f) {
-            Ok(year_number) => year_number.to_string(),
-            Err(error) => panic!("{}", format!("Cannot retrieve year number: {:#?}", error)),
-        }),
-    )
+    SortingStrategy::new("year", |f: &File, _| match get_year_number(f) {
+        Ok(year_number) => year_number.to_string(),
+        Err(error) => panic!("{}", format!("Cannot retrieve year number: {:#?}", error)),
+    })
 }
