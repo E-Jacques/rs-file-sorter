b035e5585e5ce549096aefd845b48568d1b199f0
E-Jacques <emil00j77@gmail.com>
2025-06-04 20:23:36 +0200

    feat(strategy): add catalog system
    Implement a catalog system that will simplify the add of a new strategy. Catalogs can be merged between them to construct a super-catalog used in our apps.

--- src/sorting_strategies/manipulation_catalog.rs
+++ src/sorting_strategies/manipulation_catalog.rs
@@ -1,8 +1,15 @@
 use std::{collections::HashMap, fs::File};
 
-use crate::core::sorting_strategy::{SortingStrategy, StrategyParameter};
+use crate::{
+    core::sorting_strategy::{SortingStrategy, StrategyParameter},
+    sorting_strategies::strategy_catalog::StrategyCatalog,
+};
 
-pub fn concat_strategy() -> SortingStrategy {
+pub fn get_manipulation_catalog() -> StrategyCatalog {
+    StrategyCatalog::from(vec![get_concat_strategy()])
+}
+
+fn get_concat_strategy() -> SortingStrategy {
     SortingStrategy::new(
         "concat",
         move |f: &File, parameters: &HashMap<String, StrategyParameter>| {
