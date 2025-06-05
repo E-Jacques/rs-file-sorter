923a5c07bbd5514a4e147038f3eb6f383fe2eeba
E-Jacques <emil00j77@gmail.com>
2025-06-05 19:23:57 +0200

    feat(core): add strategy validator system
    Add a system that allow to add validators to the sorting strategy. The sorting strategy struct now implement a validate method that validate the current state of the strategy from the parameters and the specified validators. Also add some unit test to elements that weren't tested in the core module. Also add implementation of Debug for StrategyParameter.

--- src/core/mod.rs
+++ src/core/mod.rs
@@ -1,3 +1,4 @@
 pub mod sorter;
 pub mod sorting_strategy;
 pub mod strategy_parameter;
+pub mod strategy_validator;
--- src/core/sorting_strategy.rs
+++ src/core/sorting_strategy.rs
@@ -3,6 +3,9 @@ use std::fs::File;
 use std::sync::{Arc, Mutex};
 
 use crate::core::strategy_parameter::StrategyParameter;
+use crate::core::strategy_validator::{
+    parameter_exists, StrategyValidator, StrategyValidatorError,
+};
 
 type SortingStrategyAction = fn(&File, &HashMap<String, StrategyParameter>) -> String;
 
@@ -11,6 +14,7 @@ pub struct SortingStrategy {
     pub action: SortingStrategyAction,
     pub name: String,
     pub parameters: HashMap<String, StrategyParameter>,
+    pub validators: Vec<StrategyValidator>,
 }
 
 impl std::fmt::Debug for SortingStrategy {
@@ -27,11 +31,37 @@ impl SortingStrategy {
             action: action,
             name: name.to_string(),
             parameters: HashMap::new(),
+            validators: vec![],
         }
     }
 
     pub fn add_parameter(&mut self, key: String, value: StrategyParameter) {
         self.parameters.insert(key, value);
+    }
+
+    pub fn add_validator(&mut self, validator: StrategyValidator) -> &mut Self {
+        let validator_already_specified = self
+            .validators
+            .iter()
+            .any(|v: &StrategyValidator| v.name == validator.name);
+        if validator_already_specified {
+            let validator_name = validator.name;
+            panic!("[Sorting Strategy] The name associated with each validator should be unique. '{validator_name}' has been specified twice.");
+        } else {
+            self.validators.push(validator);
+            self
+        }
+    }
+
+    pub fn validate(&self) -> Result<(), StrategyValidatorError> {
+        self.parameters
+            .keys()
+            .try_for_each(|name| parameter_exists(name, &self.validators))
+            .and_then(|_| {
+                self.validators
+                    .iter()
+                    .try_for_each(|validator| validator.validate(&self.parameters))
+            })
     }
 
     pub(crate) fn apply(&self, file: &File) -> String {
@@ -43,5 +73,212 @@ impl SortingStrategy {
         println!("{:#?}", result);
 
         return result;
+    }
+}
+
+#[cfg(test)]
+mod test {
+    mod test_sorting_strategy_validate {
+        use crate::core::{
+            sorting_strategy::SortingStrategy,
+            strategy_parameter::{StrategyParameter, StrategyParameterKind},
+            strategy_validator::{StrategyValidator, StrategyValidatorError},
+        };
+
+        #[test]
+        fn should_return_unknown_parameter_error_when_one_parameter_not_in_specification() {
+            let mut sorting_strategy =
+                SortingStrategy::new("my-strategy", |_, _| String::default());
+            sorting_strategy.add_validator(StrategyValidator::new(
+                "known-param",
+                StrategyParameterKind::SingleString,
+                true,
+            ));
+            sorting_strategy.add_parameter(
+                String::from("unknown-param"),
+                StrategyParameter::SingleString(String::from("My value 1")),
+            );
+            sorting_strategy.add_parameter(
+                String::from("known-param"),
+                StrategyParameter::SingleString(String::from("My value 2")),
+            );
+
+            assert_eq!(
+                sorting_strategy.validate(),
+                Err(StrategyValidatorError::UnknownParameter(String::from(
+                    "unknown-param"
+                )))
+            )
+        }
+
+        #[test]
+        fn should_return_type_error_when_parameter_kind_missmatch_with_specification() {
+            let mut sorting_strategy =
+                SortingStrategy::new("my-strategy", |_, _| String::default());
+            sorting_strategy.add_validator(StrategyValidator::new(
+                "param-1",
+                StrategyParameterKind::SingleString,
+                true,
+            ));
+            sorting_strategy.add_validator(StrategyValidator::new(
+                "param-2",
+                StrategyParameterKind::Strategy,
+                false,
+            ));
+            sorting_strategy.add_parameter(
+                String::from("param-1"),
+                StrategyParameter::SingleString(String::from("My value 1")),
+            );
+            sorting_strategy.add_parameter(
+                String::from("param-2"),
+                StrategyParameter::SingleString(String::from("My value 2")),
+            );
+
+            assert_eq!(
+                sorting_strategy.validate(),
+                Err(StrategyValidatorError::TypeError(StrategyValidator::new(
+                    "param-2",
+                    StrategyParameterKind::Strategy,
+                    false,
+                )))
+            )
+        }
+
+        #[test]
+        fn should_return_missing_mandatory_parameter_error_when_one_required_parameter_is_missing()
+        {
+            let mut sorting_strategy =
+                SortingStrategy::new("my-strategy", |_, _| String::default());
+            sorting_strategy.add_validator(StrategyValidator::new(
+                "param-1",
+                StrategyParameterKind::SingleString,
+                true,
+            ));
+            sorting_strategy.add_validator(StrategyValidator::new(
+                "param-2",
+                StrategyParameterKind::SingleString,
+                false,
+            ));
+            sorting_strategy.add_parameter(
+                String::from("param-2"),
+                StrategyParameter::SingleString(String::from("My value 2")),
+            );
+
+            assert_eq!(
+                sorting_strategy.validate(),
+                Err(StrategyValidatorError::MissingMandatoryParameter(
+                    StrategyValidator::new("param-1", StrategyParameterKind::SingleString, true,)
+                ))
+            )
+        }
+
+        #[test]
+        fn should_return_ok_if_specification_is_respected() {
+            let mut sorting_strategy =
+                SortingStrategy::new("my-strategy", |_, _| String::default());
+            sorting_strategy.add_validator(StrategyValidator::new(
+                "param-1",
+                StrategyParameterKind::SingleString,
+                true,
+            ));
+            sorting_strategy.add_validator(StrategyValidator::new(
+                "param-2",
+                StrategyParameterKind::SingleString,
+                false,
+            ));
+            sorting_strategy.add_parameter(
+                String::from("param-1"),
+                StrategyParameter::SingleString(String::from("My value 1")),
+            );
+            sorting_strategy.add_parameter(
+                String::from("param-2"),
+                StrategyParameter::SingleString(String::from("My value 2")),
+            );
+
+            assert_eq!(sorting_strategy.validate(), Ok(()))
+        }
+    }
+
+    mod test_add_validator {
+        use crate::core::{
+            sorting_strategy::SortingStrategy, strategy_parameter::StrategyParameterKind,
+            strategy_validator::StrategyValidator,
+        };
+
+        #[test]
+        #[should_panic(
+            expected = "[Sorting Strategy] The name associated with each validator should be unique. 'param-twice' has been specified twice."
+        )]
+        fn should_panic_if_validator_with_same_name_specified_twice() {
+            let mut sorting_strategy =
+                SortingStrategy::new("my-strategy", |_, _| String::default());
+            sorting_strategy.add_validator(StrategyValidator::new(
+                "param-twice",
+                StrategyParameterKind::SingleString,
+                true,
+            ));
+            sorting_strategy.add_validator(StrategyValidator::new(
+                "param-twice",
+                StrategyParameterKind::SingleString,
+                false,
+            ));
+        }
+
+        #[test]
+        fn should_push_to_the_list_of_validators() {
+            let mut sorting_strategy =
+                SortingStrategy::new("my-strategy", |_, _| String::default());
+            sorting_strategy.add_validator(StrategyValidator::new(
+                "param-1",
+                StrategyParameterKind::SingleString,
+                true,
+            ));
+            sorting_strategy.add_validator(StrategyValidator::new(
+                "param-2",
+                StrategyParameterKind::SingleString,
+                false,
+            ));
+
+            assert_eq!(
+                sorting_strategy.validators,
+                vec![
+                    StrategyValidator::new("param-1", StrategyParameterKind::SingleString, true),
+                    StrategyValidator::new("param-2", StrategyParameterKind::SingleString, false)
+                ]
+            )
+        }
+    }
+
+    mod test_add_parameter {
+        use std::collections::HashMap;
+
+        use crate::core::{
+            sorting_strategy::SortingStrategy, strategy_parameter::StrategyParameter,
+        };
+
+        #[test]
+        fn should_push_to_the_hashmap_of_parameters() {
+            let mut sorting_strategy =
+                SortingStrategy::new("my-strategy", |_, _| String::default());
+            sorting_strategy.add_parameter(
+                String::from("param-1"),
+                StrategyParameter::SingleString(String::from("My value 1")),
+            );
+            sorting_strategy.add_parameter(
+                String::from("param-2"),
+                StrategyParameter::SingleString(String::from("My value 2")),
+            );
+
+            let mut expected_parameters = HashMap::new();
+            expected_parameters.insert(
+                String::from("param-1"),
+                StrategyParameter::SingleString(String::from("My value 1")),
+            );
+            expected_parameters.insert(
+                String::from("param-2"),
+                StrategyParameter::SingleString(String::from("My value 2")),
+            );
+            assert_eq!(sorting_strategy.parameters, expected_parameters)
+        }
     }
 }
