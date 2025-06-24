c84fa8c1c985e1b0c721d1ccbe6db844cd612f63
E-Jacques <emil00j77@gmail.com>
2025-06-24 23:01:52 +0200

    feat(ui): support new error system
    Implements a mechanism to handle the new error system. Add a new widget called alert the embed a container to display some message to the end user.

--- src/ui/widgets/icon.rs
+++ src/ui/widgets/icon.rs
@@ -12,10 +12,15 @@ pub const FOLDER_CLOSED: &str = "\u{E803}";
 
 #[allow(dead_code)]
 pub const FOLDER_OPENED: &str = "\u{E804}";
+
+pub const WARNING: &str = "\u{E805}";
+pub const INFO: &str = "\u{E806}";
+pub const ERROR: &str = "\u{E807}";
+
 #[allow(dead_code)]
-pub const LOADER: &str = "\u{E805}";
+pub const LOADER: &str = "\u{E808}";
 #[allow(dead_code)]
-pub const GITHUB: &str = "\u{E806}";
+pub const GITHUB: &str = "\u{E809}";
 
 pub fn icon(code: &str) -> Text<'_> {
     text(code).font(Font::with_name("rsfs"))
