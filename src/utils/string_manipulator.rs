28e4829653987963e35f5c5e1364d464a6a3028a
E-Jacques <emil00j77@gmail.com>
2025-06-27 13:18:19 +0200

    chore: remove unused function.
    Remove the add_0_to_single_number function that were used anymore.

--- src/utils/string_manipulator.rs
+++ src/utils/string_manipulator.rs
@@ -1,13 +1,3 @@
-pub fn add_0_to_single_number(n: u32) -> String {
-    let mut n_str = n.to_string();
-    if n < 10 {
-        let mut zero_str = String::from("0");
-        zero_str.push_str(n_str.as_str());
-        n_str = zero_str.to_owned();
-    }
-
-    n_str
-}
 
 pub fn random_string(length: usize) -> String {
     let chars: Vec<char> = "abcdefghijklmnopqrstuvwxyzABCDEFGHIJKLMNOPQRSTUVWXYZ0123456789"
